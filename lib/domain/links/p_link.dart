2b2967cd8da6494717e235869c4b0e80130e5c06
Eduardo <ceduardolima2000@gmail.com>
2024-07-15 15:52:32 -0300

    Criando classe que define as posições de um Robo

--- lib/domain/links/p_link.dart
+++ lib/domain/links/p_link.dart
@@ -1,6 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:math';
-
 import 'package:r_link/domain/links/link.dart';
 import 'package:r_link/domain/utils/dh_matrix.dart';
 
---
+++ lib/domain/robot/robot_position.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:r_link/domain/comunication/transmition.dart';
+
+class RobotPosition implements Transmite {
+  final int linksQuantity;
+  final List<int> positions;
+
+  RobotPosition(this.linksQuantity, this.positions)
+      : assert(linksQuantity == positions.length,
+            "Número de juntas deve ser igual ao número de positções");
+
+  @override
+  String transmite() {
+    // TODO: implement transmite
+    throw UnimplementedError();
+  }
+
+  @override
+  String toString() => 'RobotPosition(linksQuantity: $linksQuantity, positions: $positions)';
+}
