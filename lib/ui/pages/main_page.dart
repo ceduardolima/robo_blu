31a2db95fa9bbd82cf1f0635dc481d314b637965
Eduardo <ceduardolima2000@gmail.com>
2024-07-17 08:01:25 -0300

    Criando página que realiza a conexão com via bluetooth

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:r_link/mock/robot_mock.dart';
+import 'package:r_link/ui/pages/bluetooth_connection/bluetooth_connection_page.dart';
 import 'package:r_link/ui/pages/control_position/control_positions.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
+
+  Widget buildBluetoothButton(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const BluetoothConnectionPage(),
+          ),
+        );
+      },
+      icon: const Icon(Icons.bluetooth),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       appBar: AppBar(
-          title: Text(
-        "Menu",
-        style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
-      )),
+        title: Text(
+          "Menu",
+          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
+        ),
+        actions: [buildBluetoothButton(context)],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
